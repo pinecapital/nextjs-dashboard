94b9a334f21689129e7726b1cd61383dc5d84f4f
pinecapital <154320808+pinecapital@users.noreply.github.com>
2024-04-20 22:51:33 +0530

    search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname,useRouter } from 'next/navigation';
@@ -9,20 +10,20 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const {replace} = useRouter()
 
-  function handleSearch(term: string) {
+
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-
-
+ 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     if (term) {
-      params.set('query',term);
+      params.set('query', term);
     } else {
-      params.delete('query')
+      params.delete('query');
     }
-
     replace(`${pathname}?${params.toString()}`);
-  }
+  },300)
 
 
   return (
